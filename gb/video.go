372684027fe6dbbff843ec657baf8ced5ee1fd52
gerow <gerow.mike@gmail.com>
2016-04-23 14:05:02 -0700

    added helpers to extract OAM data

--- gb/video.go
+++ gb/video.go
@@ -54,3 +54,44 @@ func (v *Video) Ws(addr uint16, val uint16) {
 func (v *Video) Asserts(addr uint16) bool {
 	return v.getHandler(addr) != nil
 }
+
+const nOAMblocks uint = 40
+const OAMblockSize uint = 4
+
+type OAMblock struct {
+	y       uint8
+	x       uint8
+	pattern uint8
+	flags   uint8
+}
+
+func (o *OAMblock) priority() bool {
+	return o.flags&0x80 != 0
+}
+
+func (o *OAMblock) yFlip() bool {
+	return o.flags&0x40 != 0
+}
+
+func (o *OAMblock) xFlip() bool {
+	return o.flags&0x20 != 0
+}
+
+func (o *OAMblock) palette() bool {
+	return o.flags&0x10 != 0
+}
+
+func (v *Video) oamBlocks() *[nOAMblocks]OAMblock {
+	i := uint(0)
+	out := [nOAMblocks]OAMblock{}
+	for n := uint(0); n < nOAMblocks; n++ {
+		out[n] = OAMblock{
+			v.oam.data[i],
+			v.oam.data[i+1],
+			v.oam.data[i+2],
+			v.oam.data[i+3]}
+		i += OAMblockSize
+	}
+
+	return &out
+}
