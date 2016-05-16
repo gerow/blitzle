c61000d72c0f209ec2c59998b49fbc31e60230f5
gerow <gerow.mike@gmail.com>
2016-05-15 17:45:29 -0700

    completed joypad test for second bank

--- gb/joypad_test.go
+++ gb/joypad_test.go
@@ -16,6 +16,11 @@ func TestButtons(t *testing.T) {
 		0x3e, 0x10, // LD A,$10
 		0xe0, 0x00, // LDH ($00),A
 		0xf0, 0x00, // LDH A,($00)
+		0xf0, 0x00, // LDH A,($00)
+		0xf0, 0x00, // LDH A,($00)
+		0xf0, 0x00, // LDH A,($00)
+		0xf0, 0x00, // LDH A,($00)
+		0xf0, 0x00, // LDH A,($00)
 	})
 	checkStep(t, s, 12) // LDH A,($00)
 	checkIP(t, s, 0x102)
@@ -126,6 +131,62 @@ func TestButtons(t *testing.T) {
 	checkIP(t, s, 0x118)
 	// Buttons should still be high since we haven't pressed one.
 	if s.cpu.a != 0xdf {
-		t.Errorf("Expected A=EFh, got %02Xh\n", s.cpu.a)
+		t.Errorf("Expected A=DFh, got %02Xh\n", s.cpu.a)
+	}
+
+	// Press start
+	state = ButtonState{}
+	state.Start = true
+	s.UpdateButtons(state)
+	checkStep(t, s, 12) // LDH A,($00)
+	checkIP(t, s, 0x11a)
+	// Bit three should be low
+	if s.cpu.a != 0xd7 {
+		t.Errorf("Expected A=D7h, got %02Xh\n", s.cpu.a)
+	}
+
+	// Press select
+	state = ButtonState{}
+	state.SelectButton = true
+	s.UpdateButtons(state)
+	checkStep(t, s, 12) // LDH A,($00)
+	checkIP(t, s, 0x11c)
+	// Bit two should be low
+	if s.cpu.a != 0xdb {
+		t.Errorf("Expected A=DBh, got %02Xh\n", s.cpu.a)
+	}
+
+	// Press B
+	state = ButtonState{}
+	state.B = true
+	s.UpdateButtons(state)
+	checkStep(t, s, 12) // LDH A,($00)
+	checkIP(t, s, 0x11e)
+	// Bit one should be low
+	if s.cpu.a != 0xdd {
+		t.Errorf("Expected A=DDh, got %02Xh\n", s.cpu.a)
+	}
+
+	// Press A
+	state = ButtonState{}
+	state.A = true
+	s.UpdateButtons(state)
+	checkStep(t, s, 12) // LDH A,($00)
+	checkIP(t, s, 0x120)
+	// Bit zero should be low
+	if s.cpu.a != 0xde {
+		t.Errorf("Expected A=DEh, got %02Xh\n", s.cpu.a)
+	}
+
+	// Press A and B, just for giggles
+	state = ButtonState{}
+	state.A = true
+	state.B = true
+	s.UpdateButtons(state)
+	checkStep(t, s, 12) // LDH A,($00)
+	checkIP(t, s, 0x122)
+	// Bit zero and two should be low
+	if s.cpu.a != 0xdc {
+		t.Errorf("Expected A=DAh, got %02Xh\n", s.cpu.a)
 	}
 }
